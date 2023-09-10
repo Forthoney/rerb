e7f7b987f343911bb41443b3039cb09956ee9048
Forthoney <castlehoneyjung@gmail.com>
2023-09-10 11:15:48 -0400

    fix transpiler nesting logic

--- lib/transpiler.rb
+++ lib/transpiler.rb
@@ -4,11 +4,25 @@ require 'better_html'
 require 'better_html/parser'
 require 'better_html/tree/tag'
 
+class Frame
+  attr_reader :name, :elems
+
+  def initialize(name)
+    @name = name
+    @elems = []
+  end
+
+  def push!(elem)
+    @elems = @elems.push(elem)
+    return
+  end
+end
+
 class Transpiler
   def initialize(source)
     @counter = 0
     @parser = create_parser(source)
-    @frames = [{ name: '@document', elems: [] }]
+    @frames = [Frame.new('document')]
   end
 
   def transpile
@@ -17,8 +31,12 @@ class Transpiler
 
   private
 
+  def current_frame
+    @frames.last
+  end
+
   def create_parser(source)
-    template = File.open(source).read
+    template = File.open(source).read.delete("\n")
     buffer = Parser::Source::Buffer.new('(buffer)')
     buffer.source = template
     BetterHtml::Parser.new(buffer)
@@ -27,19 +45,27 @@ class Transpiler
   def transpile_ast(ast)
     case extract_node_info(ast)
     in [:string, nil]
-      ast
+      [:string, "#{current_frame.name}[:innerHTML] = #{ast}\n"]
     in [:erb, _]
       transpile_erb(ast)
     in [:opening_tag, tag]
-      "#{generate_frame} = doc.createElement('#{tag.name}')\n"
+      el_name = generate_el_name
+      current_frame.push!([el_name, "#{el_name} = doc.createElement('#{tag.name}')\n"])
+      add_new_frame(el_name)
+      return
     in [:closing_tag, _]
-      frame = pop_frame
-      frame[:elems].reduce('') do |acc, elem|
-        "#{acc}#{frame[:name]}.appendChild(#{elem})\n"
-      end
+      frame = @frames.pop
+      collect_child_result(frame)
     in [:container, _]
-      content = ast.children.reduce('') { |acc, node| "#{acc}#{transpile_ast(node)}\n" }
-      insert_into_frame(content)
+      add_new_frame(current_frame.name)
+      ast.children.each do |node|
+        transpiled = transpile_ast(node)
+        current_frame.push!(transpiled) unless transpiled.nil?
+      end
+      frame = @frames.pop
+      collect_child_result(frame)
+    else
+      raise StandardError, "Failed to transpile"
     end
   end
 
@@ -60,27 +86,36 @@ class Transpiler
     end
   end
 
-  def collect_child_result(content)
-    content.reduce('') { |acc, c| acc + transpile_ast(c) }
+  def collect_child_result(frame)
+    result = frame.elems.reduce('') do |acc, elem|
+      case elem
+      in [:string, content]
+        "#{acc}#{content}"
+      in [:erb, content]
+        "#{acc}#{content}"
+      in [:container, content]
+        "#{acc}#{content}"
+      in [el_name, content]
+        "#{acc}#{content}#{frame.name}.appendChild(#{el_name})\n"
+      else
+        raise StandardError, "\n#{elem} cannot be parsed.\nCurrent frames: #{@frames}"
+      end
+    end
+    [:container, result]
   end
 
-  def generate_frame
+  def add_new_frame(name)
+    @frames = @frames << Frame.new(name)
+    return
+  end
+
+  def generate_el_name
     @counter += 1
-    frame = { name: "@el#{@counter}", elems: [] }
-    @frames << frame
-    frame[:name]
-  end
-
-  def pop_frame
-    @frames.pop
-  end
-
-  def insert_into_frame(content)
-    @frames.last[:elems] << content
+    "@el#{@counter}"
   end
 
   def transpile_erb(erb)
-    'erb'
+    [:erb, "erb\n"]
   end
 end
 
